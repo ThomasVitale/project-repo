ac4a1c0ab999bcb857dd9188be054d74896aa6a8
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2021-01-13 07:28:40 +0100

    Add 10_02/begin

---
+++ Chapter10/10_02/begin/catalog-service/src/main/java/com/polarbookshop/catalogservice/persistence/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.polarbookshop.catalogservice.persistence;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter @Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    private Long createdDate;
+
+    @LastModifiedDate
+    private Long lastModifiedDate;
+
+    @Version
+    private int version;
+}
